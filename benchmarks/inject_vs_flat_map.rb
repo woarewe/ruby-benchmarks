d14ece754d7d581a66c8a8137859f4d85205db3c
woarewe <rostislav.zhuravsky@gmail.com>
2020-02-05 13:47:42 +0300

    Add Array#map + flatten(1) to benchmark

--- benchmarks/inject_vs_flat_map.rb
+++ benchmarks/inject_vs_flat_map.rb
@@ -8,6 +8,7 @@ INNER_ARRAY_SIZE = 1000
 ary = Array.new(OUTER_ARRAY_SIZE) { Array.new(INNER_ARRAY_SIZE) { rand(INNER_ARRAY_SIZE) } }
 flat_map_result = nil
 inject_result = nil
+map_plus_flatten_result = nil
 
 Benchmark.ips do |x|
   x.report('Array#flat_map') do
@@ -18,7 +19,13 @@ Benchmark.ips do |x|
     inject_result = ary.inject([]) { |result, current| result + current }.uniq
   end
 
+  x.report('Array#map + flatten(1)') do
+    map_plus_flatten_result = ary.map(&:itself).flatten(1)
+  end
+
   raise 'Different result values' if inject_result != flat_map_result
+  raise 'Different result values' if map_plus_flatten_result != flat_map_result
+  raise 'Different result values' if inject_result != map_plus_flatten_result
 
   x.compare!
 end
